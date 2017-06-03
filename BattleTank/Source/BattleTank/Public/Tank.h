2a562688f0abdfdaf0ec78bfcb03cb8421248203
Rungsimun Saenprasert <s7070099@kmitl.ac.th>
2017-06-03 18:04:55 +0700

    BT31 BlueprintCallable()

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -17,6 +17,9 @@ public:
 	
 	void AimAt(FVector HitLocation);
 
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
